c930d6821c62264ba4dde148ab6393135dd6a823
Alexey Sokolov <sokolik1983@mail.ru>
2024-08-15 10:51:00 +0300

    save changes

--- src/components/MainPage/helpers.ts
+++ src/components/MainPage/helpers.ts
@@ -20,7 +20,7 @@ const colorCodes = [
  */
 export const fetchData = async () => {
     try {
-        const response = await fetch('https://financialmodelingprep.com/api/v3/search?query=AA&apikey=FTpDDg9vsIYNf2Ben0i3M0gulIBzRFgS&limit=5');
+        const response = await fetch('https://financialmodelingprep.com/api/v3/search?query=AA&apikey=FTpDDg9vsIYNf2Ben0i3M0gulIBzRFgS');
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
